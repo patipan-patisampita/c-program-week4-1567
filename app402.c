93161a2751fe00a2bcba408bb34eaec76f04c8b3
patipan <patipan.patisampita@gmail.com>
2024-06-14 15:50:43 +0700

    update

---
+++ app402.c
@@ -0,0 +1,5 @@
+#include <stdio.h>
+void main()
+{
+    printf("%s %d %5.2f", "Hello", 12, 20.5);
+}
---
+++ app403.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+void main()
+{
+    printf("\a\a\a\a\a\n");
+    printf("%d \\ %c \: %o \/ %x", 65, 65, 65, 65);
+}
